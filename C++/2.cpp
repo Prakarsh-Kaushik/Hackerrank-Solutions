ce27eec7c6fa9fd79c18a4784d572e9103091eb0
Prakarsh-Kaushik <prakarsh.kr@gmail.com>
2021-08-26 19:28:41 +0530

    Problem 2

---
+++ C++/2.cpp
@@ -0,0 +1,19 @@
+//Problem 2 C++(Hackerrank)
+//Read  3 numbers from stdin and print their sum to stdout.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c,sum;
+	cin>>a>>b>>c;
+	sum = a+b+c;
+	cout<<sum;
+    
+	return 0;
+}
