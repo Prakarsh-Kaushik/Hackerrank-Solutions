986dcc342ce5f1d3447be5d64468eea349861004
Prakarsh-Kaushik <prakarsh.kr@gmail.com>
2021-08-30 18:42:59 +0530

    Problem 6

--- C++/6.cpp
+++ C++/6.cpp
@@ -4,21 +4,17 @@
 #include<iostream>
 #include <stdio.h>
 #include<math.h>
-using namespace std;
+void update(int *a,int *b) {
+    *a+=*b;
+    *b=abs(*a-2**b); 
+}
 
-                void fun(int *a , int *b) 
-                {
-                *a=*a+*b;
-                *b= *a-(*b+*b);
-                }
-
-int main() 
-{
+int main() {
     int a, b;
     int *pa = &a, *pb = &b;
     
     scanf("%d %d", &a, &b);
-    fun(int *a, int *b);
+    update(pa, pb);
     printf("%d\n%d", a, b);
 
     return 0;
