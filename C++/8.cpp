c6264596f384233031b6a148d9570c63b8fb4596
Prakarsh-Kaushik <prakarsh.kr@gmail.com>
2021-09-03 19:27:12 +0530

    Problem 8

---
+++ C++/8.cpp
@@ -0,0 +1,28 @@
+//Problem 8 C++(Hackerrank)
+//You will be given an array of N integers and you have to print the integers in the reverse order.
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+      
+    int n;
+    cin>>n;
+    
+    int a[n];
+    for(int i =0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    for (int i=n-1; i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+    
+    return 0;
+}
