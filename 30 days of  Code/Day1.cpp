c778a854015c19273b77a43396f3905550d7d90e
Prakarsh-Kaushik <prakarsh.kr@gmail.com>
2021-09-13 19:17:18 +0530

    Day 1

---
+++ 30 days of  Code/Day1.cpp
@@ -0,0 +1,20 @@
+//30 Days of Code (Hackerrank) C++
+//Day 1
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    string message;
+    getline(cin,message); //Takes string input in C++
+    cout<<"Hello, World."<<endl<<message;
+    
+     
+    return 0;
+}
